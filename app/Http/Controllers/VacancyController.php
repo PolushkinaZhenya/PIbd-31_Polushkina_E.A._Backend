47abeea6dfbadc46e07a7c72df00696807b6dd5d
Zhenya <polushkina.zhenya@mail.ru>
2019-11-15 08:42:07 +0300

    Lab_10_WebHook_v.7

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -186,8 +186,16 @@ class VacancyController extends Controller
         }
         else{
             $message = json_encode((string)$request->getContent());
-            $client = new Client('ws://labourexchangewebsocket.herokuapp.com/');
-            $client->send($message);
+            $array = explode('\r\n',$message['body']);
+            if(($array[0] == 'Вакансия') && (count($array) >= 4)){
+                $vacancy = Vacancy::create([
+                    'position' => $array[1],
+                    'description' => $array[2],
+                    'salary' => $array[3]
+                ]);
+                $client = new Client('ws://labourexchangewebsocket.herokuapp.com/');
+                $client->send($message);
+            }
             return 'ok';
         }
     }
